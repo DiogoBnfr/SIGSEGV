92238e008b3852710a615a32a0b3b0976d9a8dc1
vaeep <diogobonofre@gmail.com>
2024-10-28 02:05:49 -0300

    Revert "feat: Added article MongoDB model"

--- sigsegv_api/models/article.py
+++
@@ -1,7 +0,0 @@
-from mongoengine import Document, StringField, DateTimeField
-
-class Article(Document):
-    title = StringField(required=True)
-    author = StringField(required=True)
-    content = StringField(required=True)
-    date = DateTimeField(auto_now_add=True)
