cba2f8cb799d4115eeb563de514fa4dbf00022c5
John <jonatascleytonf@gmail.com>
2024-10-28 17:03:52 -0300

    Adicionado rule de delete e correções e sintaxe

--- sigsegv_api/models/article.py
+++ sigsegv_api/models/article.py
@@ -1,7 +1,7 @@
 from mongoengine import Document, LazyReferenceField, StringField, DateTimeField
 
 class Article(Document):
-    title = StringField(max_lenght=100, required=True)
-    author = LazyReferenceField('User', required=True)
+    title = StringField(max_length=100, required=True)
+    author = LazyReferenceField('User', required=True, reverse_delete_rule=CASCADE)
     content = StringField(required=True)
     date = DateTimeField(auto_now_add=True)
--- sigsegv_api/models/comment.py
+++ sigsegv_api/models/comment.py
@@ -1,7 +1,7 @@
 from mongoengine import LazyReferenceField, Document, StringField, DateTimeField
 
 class Comment(Document):
-    author = LazyReferenceField('User', required=True)
     content = StringField(max_length=200,required=True)
     date = DateTimeField(auto_now_add=True)
-    article = LazyReferenceField('Article', required=True)
+    author = LazyReferenceField('User', required=True, reverse_delete_rule=CASCADE)
+    article = LazyReferenceField('Article', required=True, reverse_delete_rule=CASCADE)
