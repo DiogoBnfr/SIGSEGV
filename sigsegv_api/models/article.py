19feaf59e030f74b1bbdecde2367d1bf847b6f44
John <jonatascleytonf@gmail.com>
2024-10-31 20:23:52 -0300

    fix: adiciei o field de comentário em article model

--- sigsegv_api/models/article.py
+++ sigsegv_api/models/article.py
@@ -1,7 +1,10 @@
 from mongoengine import Document, LazyReferenceField, StringField, DateTimeField, CASCADE
+from .user import User
+from .comment import Comment
 
 class Article(Document):
     title = StringField(max_length=100, required=True)
     author = LazyReferenceField('User', required=True, reverse_delete_rule=CASCADE)
     content = StringField(required=True)
     date = DateTimeField(auto_now_add=True)
+    comment = LazyReferenceField('Comment')
